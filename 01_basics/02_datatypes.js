ab776801c777e07a20727a7ff4805a0ee1232011
diya26105115-rgb <diya26105115@gmail.com>
2026-01-06 18:04:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat ll JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should high
+    
+    console.log("Diya") 
+
+
+    let name = "Diya"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+
+//number => 2 to power 53
+//bigint 
+// string => ""
+//boolen => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log (typeof null); //object
